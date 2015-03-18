cad540fcc97600cc3ea20c03695530f12565ae88
Zander Baldwin <hello@zanderbaldwin.com>
2015-03-18 16:11:17 +0000

    Update Main Execution Script

--- learn.php
+++ learn.php
@@ -1,27 +1,27 @@
 <?php
 
-    use \Nosco\Learn\Calculator\Calculator;
-    use \Nosco\Learn\Interfaces\Calculator\CalculatorInterface as ICalc;
+use Nosco\Learn\Calculator\Calculator;
+use Nosco\Learn\Interfaces\Calculator\CalculatorInterface as ICalc;
 
-    require_once __DIR__ . '/vendor/autoload.php';
-    require_once __DIR__ . '/functions.php';
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/functions.php';
 
-    $calculator = new Calculator;
-    if(!$calculator instanceof ICalc) {
-        throw new Exception('Could not create a correct instance of the calculator.');
-    }
+$calculator = new Calculator;
+if (!$calculator instanceof ICalc) {
+    throw new RuntimeException('Could not create a correct instance of the calculator.');
+}
 
-    $halfNumber = $calculator->createNumber(4);
-    $bigNumber = addTwoNumbers($halfNumber, $halfNumber);
+$halfNumber = $calculator->createNumber(4);
+$bigNumber = addTwoNumbers($halfNumber, $halfNumber);
 
-    $mediumNumber = $calculator->createNumber(5);
-    $smallNumber = $calculator->subtract($bigNumber, $mediumNumber);
+$mediumNumber = $calculator->createNumber(5);
+$smallNumber = $calculator->subtract($bigNumber, $mediumNumber);
 
-    $expectedResult = '24';
-    $actualResult = $calculator->multiply($bigNumber, $smallNumber);
+$expectedResult = '24';
+$actualResult = $calculator->multiply($bigNumber, $smallNumber);
 
-    echo "Results\n=======\n\n";
-    echo 'Expected: ' . $expectedResult . "\n";
-    echo 'Actual:   ' . $actualResult->getValue() . "\n\n";
-    echo 'The results are equal:     ' . ($actualResult->isEqual($expectedResult) ? 'true' : 'false') . "\n";
-    echo 'The results are identical: ' . ($actualResult->isIdentical($expectedResult) ? 'true' : 'false');
+echo "Results\n=======\n\n";
+echo sprintf("Expected: %s\n", $expectedResult);
+echo sprintf("Actual:   %s\n\n", $actualResult->getValue());
+echo sprintf("The results are equal:     %s\n", $actualResult->isEqual($expectedResult) ? 'true' : 'false');
+echo sprintf("The results are identical: %s", $actualResult->isIdentical($expectedResult) ? 'true' : 'false');
