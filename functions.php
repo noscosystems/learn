3d34a2eb846f579744f82417193efa09ed097f14
Zander Baldwin <hello@zanderbaldwin.com>
2015-03-18 16:11:57 +0000

    Update Functions

--- functions.php
+++ functions.php
@@ -1,22 +1,20 @@
 <?php
 
-    use \Nosco\Learn\Calculator\Calculator;
-    use \Nosco\Learn\Calculator\Number;
-    use \Nosco\Learn\Interfaces\Calculator\CalculatorInterface as ICalc;
-    use \Nosco\Learn\Interfaces\Calculator\NumberInterface as INum;
+use Nosco\Learn\Interfaces\Calculator\CalculatorInterface as ICalc;
+use Nosco\Learn\Interfaces\Calculator\NumberInterface as INum;
 
-    /**
-     * Add Two Numbers
-     *
-     * Adds two numbers together using the calculator provided.
-     *
-     * @access global
-     * @param  Nosco\Learn\Interfaces\Calculator\CalculatorInterface $factory
-     * @param  Nosco\Learn\Interfaces\Calculator\NumberInterface $first
-     * @param  Nosco\Learn\Interfaces\Calculator\NumberInterface $second
-     * @return Nosco\Learn\Interfaces\Calculator\NumberInterface
-     */
-    function addTwoNumbers(ICalc $factory, INum $first, INum $second)
-    {
-        return $factory->add($first, $second);
-    }
+/**
+ * Add Two Numbers
+ *
+ * Adds two numbers together using the calculator provided.
+ *
+ * @access public
+ * @param  \Nosco\Learn\Interfaces\Calculator\CalculatorInterface $factory
+ * @param  \Nosco\Learn\Interfaces\Calculator\NumberInterface $first
+ * @param  \Nosco\Learn\Interfaces\Calculator\NumberInterface $second
+ * @return \Nosco\Learn\Interfaces\Calculator\NumberInterface
+ */
+function addTwoNumbers(ICalc $factory, INum $first, INum $second)
+{
+    return $factory->add($first, $second);
+}
